c0cf37b017fa8abfd3e52adecbe4cede77090344
Kai de Haan <dehaan@sipgate.de>
2016-02-09 16:43:44 +0100

    swap arguments around again. This will allow easy autocompletion later on

--- ansishell.py
+++ ansishell.py
@@ -23,8 +23,8 @@ config = dotfile.options("config")
 
 # TODO check for missing defaults
 
-argparser.add_argument("group", help="ansible group")
-argparser.add_argument("env", help="name of environment", default="default", nargs="?")
+argparser.add_argument("env", help="name of environment", default="default")
+argparser.add_argument("group", help="ansible group", nargs="?")
 argparser.add_argument("-l", help="name of environment", action="store_true")
 args = argparser.parse_args()
 
@@ -35,16 +35,16 @@ inventory_path = dotfile.get("inventory", env)
 shell_command = dotfile.get("config", "command")
 
 inventory_manager = inventory.Inventory(inventory_path)
-hosts = inventory_manager.list_hosts(group)
 
 if args.l:
-    inventory_path = dotfile.get("inventory", group) # actually is the env parameter now
     inventory_manager = inventory.Inventory(inventory_path)
     print("Groups in %s:" % inventory_path)
     for group_item in inventory_manager.list_groups():
         print(group_item)
     exit(0)
 
+
+hosts = inventory_manager.list_hosts(group)
 if len(hosts) == 0:
     print("No hosts found")
     exit(1)
